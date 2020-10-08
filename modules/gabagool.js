061e9401a32d47dfadb84fdbe14eb4deeb2795c3
Darren <d>
2020-10-08 23:48:19 +0100

    added failsafe for consecutive gabagools

--- modules/gabagool.js
+++ modules/gabagool.js
@@ -11,8 +11,15 @@ function gabagool(message) {
 	message.channel.fetchMessages({ limit: 2 })
 		.then(lastMessages => {
 			const lastMessage = lastMessages.last().content;
+
+			// failsafe for new channels
 			if (!lastMessage) return;
-			const gabagooledMessage = `:pinched_fingers: ${lastMessage.replace(/ /g, ' :pinched_fingers: ')} :pinched_fingers:`;
+
+			// if there's 2 or more :pinched_fingers:, poop the author for their transgressions
+			const pinchedFingersCount = (lastMessage.match(/:pinched_fingers:/g) || []).length;
+			if (pinchedFingersCount > 1) return message.react('💩');
+
+			const gabagooledMessage = `:pinched_fingers: ${lastMessage.replace(/ /g, ' :pinched_fingers: ')} :pinched_fingers:`;			
 			message.channel.send(gabagooledMessage);
 		})
 		.catch(console.error);
