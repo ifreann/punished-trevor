e738da4a54e157aa644bc16db2175a26ae47c1f3
Darren <d>
2022-08-05 14:49:39 +0100

    added stream links

--- modules/evoWhen.js
+++ modules/evoWhen.js
@@ -7,12 +7,18 @@ function evoWhen(message) {
 
 	const now = dayjs();
 	const evo = dayjs(1659747600000);
+	const evoSunday = dayjs(1659848400000);
 	const evoEnds = dayjs(1659916800000);
 
 	// if evo is over
-	if (now.isAfter(evoEnds)) return message.react(`${getEmoji('despair')}`);
+	if (now.isAfter(evoEnds)) {
+		return message.react(`${getEmoji('despair')}`);
+	}
 	// if evo is underway
-	else if (now.isAfter(evo)) return message.channel.send(`Evo is underway ${getEmoji('apusmile')}👍 https://www.twitch.tv/evo`);
+	else if (now.isAfter(evo)) {
+		const stream = `https://www.twitch.tv/evo${now.isAfter(evoSunday) ? '' : 3}`;
+		return message.channel.send(`Evo is underway ${getEmoji('apusmile')}👍 ${stream}`);
+	}
 
 	const days = evo.diff(now, 'day');	
 	const hours = evo.diff(now, 'hour') - (days * 24);
