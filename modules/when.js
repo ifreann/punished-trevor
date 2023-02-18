4e624592e59b2a0d06375025e50cd0d863ddd08f
Darren <d>
2023-02-18 04:55:27 +0000

    improved error handling. Fixed blank screenshot of errors. Added delay to input.type to improve consistency with search results

--- modules/when.js
+++ modules/when.js
@@ -5,55 +5,72 @@ import puppeteer from 'puppeteer';
 // flag to prevent requests being stacked
 let busy = false;
 
+// set to true to allow local testing. Enables headful Chromium, prevents browser.close and disables discordjs interactions
+const testing = false;
+if (testing) when({content: '!when morris'});
+
 async function when(message) {
 
-	if (busy) return message.channel.send(`Please wait, I'm processing someone else's request <:pepeW:1040297494929743892> 👍`);
-	
+	if (busy && !testing) return message.channel.send(`Please wait, I'm processing someone else's request <:pepeW:1040297494929743892> 👍`);
 	busy = true;
 
 	// remove the !when from the query string
 	const query = message.content.slice(6).split(' ').join(' ');
 
 	// feedback for anxious bot users
-	message.channel.send(`Searching yourcountdown.to for "${query}". Please be patient <:apusmile:908706192107393074> 👍`);
+	if (testing) console.log(`Searching yourcountdown.to for "${query}"`);
+	else message.channel.send(`Searching yourcountdown.to for "${query}". Please be patient <:apusmile:908706192107393074> 👍`);
 
 	// args required by Railway for Puppeteer to work
-	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+	const browser =
+		testing ? await puppeteer.launch({headless: false, args: [`--window-position=500,0`]})
+		: await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 	const page = await browser.newPage();
 
 	// go to site
-	await page.goto('https://yourcountdown.to/', { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
+	await page.goto('https://yourcountdown.to/');
 
 	// accept cookies
+	const acceptButton = 'button[mode="primary"]';
+	await page.waitForSelector(acceptButton);
+	await page.click(acceptButton);
+
+	// type query into search box. Doesn't work consistently because of a bug, more info here: https://github.com/puppeteer/puppeteer/issues/1648#issuecomment-431755748
+	await page.type('#GlobalSearch', query, {delay: 100});
+
+	// remove any search results that are not countdowns
+	let numberOfResults;
 	try {
-		const acceptButton = 'button[mode="primary"]';
-		await page.waitForSelector(acceptButton, { timeout: 1000 });
-		await page.click(acceptButton);
+		await page.waitForSelector('.results-container > a', { timeout: 1000 });
+		numberOfResults = await page.evaluate(() => {
+			[...document.querySelector('.results-container').children]
+				.filter(v => !v.innerHTML.includes(`red \">Countdown</span>`))
+				.forEach(v => v.remove());
+			return document.querySelector('.results-container').children.length;
+		});
 	}
 	catch (e) {
-		// skip looking for the cookies button, it was probably already clicked
+		if (testing) return console.log('No results found.');
+		else return disappointUser(message, browser, 1);
 	}
 
-	// type query into search box
-  await page.type('#GlobalSearch', query);
+	// look for a search result. If nothing is there, it probably doesn't exist
+	if (!numberOfResults) {
+		if (testing) return console.log('No results found after deleting non-countdowns.');
+		else return disappointUser(message, browser, 2);
+	}
 
-	// wait half a second before proceeding, the site can show weird results if you go too fast
-	await new Promise(r => setTimeout(r, 500));
-
-	// wait 3 seconds for a search result. If nothing appears, it probably doesn't exist
+	// click the first search result and wait until it's loaded
 	try {
-		await page.waitForSelector('.results-container > a', { timeout: 3000 });
+		const href = await page.evaluate(() => document.querySelector('.results-container > a').href);
+		await page.goto(href, { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
 	}
 	catch (e) {
-		busy = false;
-		return message.channel.send(`No countdown found. It's either already out or the site is broken. Or you typed something dumb <:pepeW:1040297494929743892>`);
+		if (testing) return console.log(`Couldn't get href, giving up.`);
+		else return disappointUser(message, browser, 3);
 	}
 
-	const href = await page.evaluate(() => document.querySelector('.results-container > a').href);
-
-	await page.goto(href, { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
-
-	// sets page to a good width for Discord. Height doesn't matter
+	// sets page to a good width for Discord screenshots. Height doesn't matter
 	await page.setViewport({width: 770, height: 1200});
 
 	// remove elements interfering with screenshot (cookies overlay, ads, buttons)
@@ -69,14 +86,36 @@ async function when(message) {
 	// if the element wasn't found, take a screenshot of the page and post it instead
 	if (!element) {
 		const screenshot = await page.screenshot();
-		message.channel.send(`Something killed me. Here's the last thing I saw before I died:`, {files: [{ attachment: screenshot }]});
+		if (testing) {
+			await page.screenshot({path: '_error.png'});
+			console.log('Failed at screenshot. Error saved as _error.png');
+		}
+		else message.channel.send(`Something killed me. Here's the last thing I saw before I died:`, {files: [{ attachment: screenshot }]});
 	}
 	else {
 		const screenshot = await element.screenshot();
-		message.channel.send({files: [{ attachment: screenshot }]});
+		if (testing) {
+			await element.screenshot({path: '_result.png'});
+			console.log('Result saved as _result.png');
+		}
+		else message.channel.send({files: [{ attachment: screenshot }]});
 	}
 
-	await browser.close();
+	end(browser);
+	if (testing) console.log('Finished');
+
+}
+
+async function end(browser) {
+	
+	if (!testing) await browser.close();
 	busy = false;
+
+}
+
+function disappointUser(message, browser, code) {
+
+	message.channel.send(`No countdown found. It's either already out or the site is broken. Or you typed something dumb <:pepeW:1040297494929743892>`);
+	end(browser);
 
 }
