a2651b8727700afeb3253a719fffc9030e522739
Darren <d>
2023-11-20 17:28:05 +0000

    added omegaping

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ import say from './modules/say.js';
 import slots from './modules/slots.js';
 import utOgh from './modules/utOgh.js';
 import togglePin from './modules/togglePin.js';
+import omegaPing from './modules/omegaPing.js'
 
 // generic error/warning handling so Trevor doesn't just crash
 client.on('error', e => console.error(e));
@@ -29,14 +30,10 @@ client.on('messageCreate', message => {
 	const authorIsDarren = message.author.id === '71612859766800384';
 	const authorIsMike = message.author.id === '141641930349084672';
 	const authorIsHarry = message.author.id === '210155641468223499';
-	const trevorBetaIsOnline = checkIfTrevorBetaIsOnline(message);
 	const { content } = message;
 
 	// ignore Trevor
 	if (authorIsTrevor) return;
-
-	// do nothing if beta trevor is also taking commands in the trevordome
-	if (!trevorBetaIsOnline) return;
 
 	// text match triggers
 	if (content.match(/^!e /) && authorIsDarren) evaluate(message, client);
@@ -51,26 +48,15 @@ client.on('messageCreate', message => {
 	else if (content.match(/widepeepoHappy/i)) postImage(message, 'widepeepoHappy');
 	else if (content.match(/pepeLaugh/i)) postImage(message, 'pepeLaugh');
 	else if (content.match(/mikefoot/i)) postImage(message, 'mikefoot');
-	else if (content === '!slots') slots(message);
+	else if (content.match(/^!slots$/i)) slots(message);
 	else if (content.match(/\bu+\s*t+\s*o+\s*g+\s*h+\b/i)) utOgh(message);
 	else if (content.match(/^mock/i)) mock(message);
 	else if (content.match(/\b(good|bad)\s*bot\b/i)) acceptCriticism(message);
 	else if (content.match(/^!eoin$/i)) postImage(message, 'eoin');
 	else if (content.match(/^!shad$/i)) postImage(message, 'shad');
+	else if (content.match(/^!omegaping/i)) omegaPing(message);
 	
 	// always has a chance to trigger
 	if (authorIsMike) poopMike(message);
 
 });
-
-function checkIfTrevorBetaIsOnline(message) {
-
-	// if trevor beta is doing this, skip this function. This should only be done by main trevor, and only in the Trevordome
-	if (client.user.id === '1013832791420588223' && message.channel.id === '517382587090731008') return false;
-
-	// returns true if 1013832791420588223 (trevor beta) is online. Returns false for anything else
-	return message.guild.members.fetch('1013832791420588223')
-		.then(user => user.presence.status === 'online')
-		.catch(e => false);
-
-}
