3fa3117c0cedc86b183085f3c8e5a9217f057989
Darren <darren1337@gmail.com>
2026-01-08 02:04:58 +0000

    updated !visit

--- modules/visit.js
+++ modules/visit.js
@@ -6,7 +6,7 @@ import fromUnixTime from "date-fns/fromUnixTime/index.js";
 
 function visit(message) {
 
-	const evoDate = fromUnixTime(1763247300);
+	const evoDate = fromUnixTime(1768698000);
 
 	const diff = differenceInSeconds(evoDate, new Date());
 	const days = Math.floor(diff / 86400);
